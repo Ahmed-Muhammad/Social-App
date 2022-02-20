169985a7314d525e06e0da21fe59b73fa66b1d1d
Ahmad M. Hassanien <44645999+Ahmed-Muhammad@users.noreply.github.com>
2022-02-20 08:04:22 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()  async{
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   DatabaseReference ref;
   ref = FirebaseDatabase.instance.ref().child('product');
-  ref.set(10);
+  ref.set(100);
 
   runApp(const MyApp());
 }
